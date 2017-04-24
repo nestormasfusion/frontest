943f054def007789bfbb804b37b197fdf5250805
alberto <jose.aguilar@sistemabea.mx>
2017-04-24 17:47:54 -0500

    #frontest-alberto - Proyecto Alberto Aguilar

---
+++ templates/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FrontEnd - Alberto Aguilar</title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAITzePrY8VaEfsKZFLcbdzB7XKZsvwTuY"></script>
+    <style>
+	  .carousel-inner > .item > img,
+	  .carousel-inner > .item > a > img {
+	      width: 100%;
+	      margin: auto;
+	  }
+	  	.logo{
+		  	color: #136DC8;
+		    font-size: 40px;
+		    margin: 0;
+		    font-weight: bold;
+		    text-decoration: none;
+		}
+		.logo:hover{
+			text-decoration: none;
+		}
+	 </style>
+</head>
+<body>
+	<div class="header-top">
+		<div class="container">
+			<div class="col-md-6"></div>
+			<div class="col-md-6 text-right">
+				<p>
+					<i class="fa fa-phone icon-class" aria-hidden="true"></i>CDMX   (55) 0000 0000  |   
+					<i class="fa fa-phone icon-class" aria-hidden="true"></i>GDL   (33) 0000 0000   |   
+					<a href="#" title="Facebook" target="_blank"><i style="color: #FFF" class="fa fa-facebook icon-class" aria-hidden="true"></i></a>
+					<a href="#" title="Twitter" target="_blank"><i style="color: #FFF" class="fa fa-twitter icon-class" aria-hidden="true"></i></a></p>
+			</div>
+		</div>
+	</div>
+	<nav class="gray-color">
+		<div class="container">
+		  	<div class="col-md-6">
+		  		<div class="navbar-header">
+				     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				    </button>
+				    <h1 class="logo">BUILD</h1>
+				</div>
+		  	</div>
+			<div class="col-md-6">
+		  		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      <ul class="nav navbar-nav navbar-right">
+			        <li><a id="m-proyectos" href="#">PROYECTOS</a></li>
+			        <li><a id="m-nosotros" href="#">NOSOTROS</a></li>
+			        <li><a id="m-servicios" href="#">SERVICIOS</a></li>
+			        <li><a id="m-contactanos" href="#">CONTÁCTANOS</a></li>
+			      </ul>
+			    </div>
+			</div>
+		</div>
+	</nav>
